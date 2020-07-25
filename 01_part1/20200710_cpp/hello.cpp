d91fe27a838e9ee8d0d0704365d0caa3b1f14cf8
YUKI-SOKENDAI <abeyuki@post.kek.jp>
2020-07-25 18:17:04 +0900

    initial set.

---
+++ 01_part1/20200710_cpp/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void){
+
+std::cout <<"hoge moge foo bar..."<<std::endl;
+
+return 0;
+
+}
---
+++ 02_part2/class_prac.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+
+int main(void){
+
+for(i=0;)
+
+return 0;
+}
